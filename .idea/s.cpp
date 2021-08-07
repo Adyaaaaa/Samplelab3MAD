0c9268bfd4e45db7833dc57b29a7ee4a5606ddea
Anupazen <anupasenevirathne@gmail.com>
2021-08-07 13:12:09 +0530

    tc2

---
+++ .idea/s.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ACER on 8/7/2021.
+//
+
+#include "s.h"
---
+++ .idea/s.h
@@ -0,0 +1,16 @@
+//
+// Created by ACER on 8/7/2021.
+//
+
+#ifndef SAMPLELAB3MAD_S_H
+#define SAMPLELAB3MAD_S_H
+
+
+
+class s {
+
+};
+
+
+
+#endif //SAMPLELAB3MAD_S_H
